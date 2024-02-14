3540980aa0b7f2c5a5fa3e9e0c20678cd0cf40dd
manasanttdata <154868339+manasanttdata@users.noreply.github.com>
2024-02-14 09:59:59 +0530

    Update ValuesController.cs

--- AWSServerless_03/Controllers/ValuesController.cs
+++ AWSServerless_03/Controllers/ValuesController.cs
@@ -9,7 +9,7 @@ public class ValuesController : ControllerBase
     [HttpGet]
     public IEnumerable<string> Get()
     {
-        return new string[] { "value1", "value2" };
+        return new string[] { "Hello", "World!" };
     }
 
     // GET api/values/5
